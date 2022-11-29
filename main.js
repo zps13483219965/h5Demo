0f9e60b8c09896f2d365d9849e4ee6310f0483d8
zps13483219965 <87683609+zps13483219965@users.noreply.github.com>
2022-11-29 19:41:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+import { createApp } from 'vue'
+
+// 引入后，vant样式有问题
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+import directive from './directive' // directive
+import vant from './plugins/vant' // vant
+
+// 注册指令
+import plugins from './plugins' // plugins
+
+import components from '@/components' // 全局注册所有组件
+
+// 路由权限
+import '@/permission' // permission control
+
+import 'lib-flexible/flexible'
+
+// 等比适配文件
+import "@/utils/rem"
+
+// 引入bridge.js 全局使用
+import "@/utils/bridge"
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(plugins)
+
+app.use(components)
+
+app.use(vant)
+
+directive(app)
+
+app.mount('#app')
